24c6b2bf2b16dd28a6819c02cb2905a6cd840927
. <.>
2025-03-14 18:04:47 +0000

    models

--- Models/Reservations.cs
+++ Models/Reservations.cs
@@ -1,14 +1,22 @@
-﻿namespace Reservation.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Reservation.Models
 
 {  
     public class Reservations
     {
         public int Id { get; set; }
         public int ClientId { get; set; }
+        [ForeignKey("ClientId")] 
         public Client Client { get; set; }
+
+        
         public int VolId { get; set; }
+        [ForeignKey("VolId")] 
         public Vol Vol { get; set; }
-        public EtatReservation Etat { get; set; } // État de la réservation
+
+       
+        public EtatReservation Etat { get; set; }
         public DateTime DateReservation { get; set; }
     }
     public enum EtatReservation
